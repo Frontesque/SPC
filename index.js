e11380bb4316ebedc5df10bbc26007dca9c5b692
Front <27463495+Frontesque@users.noreply.github.com>
2021-10-16 18:58:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //---   Modile Imports   ---//
 const request = require('request');
 const fs = require('fs');
+require('colors');
 
 //---   Proxy Imports   ---//
 const proxyFile = fs.readFileSync('proxies.txt', 'utf8');
@@ -10,16 +11,23 @@ const proxies = proxyFile.split('\r\n')
 for (const i in proxies) {
     const proxy = proxies[i];
 
+    console.log("Attempting Proxy:".bold.magenta,proxy)
 
-    request({
-        'url':'https://api.ipify.org?format=json',
-        'method': "GET",
-        'proxy': 'http://'+proxy,
-    },function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        }
-    })
+    try {
+        request({
+            'url':'https://api.ipify.org?format=json',
+            'method': "GET",
+            'proxy': 'http://'+proxy,
+        },function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('Proxy Success:'.bold.green,proxy);
+            } else {
+                console.log('Proxy Fail:'.bold.red,proxy);
+            }
+        })
+    } catch {
+        console.log('Proxy Hard Fail:'.bold.red,proxy);
+    }
 
 
 }
